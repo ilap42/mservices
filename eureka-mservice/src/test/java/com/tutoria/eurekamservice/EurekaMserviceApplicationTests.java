81113545a0ebdf6c3a87bbd228cb0f087abd0b2c
ilap42 <ilap435@gmail.com>
2021-09-16 17:31:14 -0500

    eureka

---
+++ eureka-mservice/src/test/java/com/tutoria/eurekamservice/EurekaMserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutoria.eurekamservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaMserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- user-mservice/src/main/java/com/tutoria/user/mservice/UserMserviceApplication.java
+++ user-mservice/src/main/java/com/tutoria/user/mservice/UserMserviceApplication.java
@@ -2,10 +2,12 @@ package com.tutoria.user.mservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableEurekaClient
 public class UserMserviceApplication {
 
 	public static void main(String[] args) {
